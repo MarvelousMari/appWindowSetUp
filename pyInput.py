dbd7572e4784597a71a48f7cd4b137af3f61896a
MarvelousMari <mariella@mariellapage.com>
2020-07-10 12:45:01 -0500

    Finished a non gui version that works needs testing on duplicates, also comments need to be redone to match the new formats for the dictionaries

--- pyInput.py
+++ pyInput.py
@@ -4,25 +4,32 @@
 
 import subprocess
 import time
+import os
 
 # wait time that can be set if needed cause of programs launching to slowly
-waitTime = 5
+waitTime = 3
 
 # get applications to launch
 print("please enter the applications you would like to launch one at a time")
 print("when you're done entering applications input 'DONE'")
+print("use the format for launching the application in terminal including any arguments you use:")
+print("")
+
+# list of launch cmds
 appList = []
 while True:
     # make sure it's in proper terminal format
-    app = input("use the format for launching the application in terminal including any arguments you use:\
-    ")
+    app = input("cmd:  ")
+    print("")
     # TODO: add while catch correct input to prevent mistakes
     if app == 'DONE':
         break
     else:
         appList.append(app)
 
-# dict for launch app cmd : window name
+# print("applist: %s" % (appList))
+
+# dict for window name : app launch cmd
 windowNames = {}
 
 # get the window names of all of the apps
@@ -36,14 +43,18 @@ for app in appList:
     # else ask for what part of the app window header is always there
     else:
         print("what part of this line: ")
+        # stdout of ./appLaunch
         print(appSubProc.stdout.lower())
         print("is always in the header of the %s window: " % (app))
-        windowNames[app] = input().lower()
+        windowNames[input().lower()] = app
 
-print("windowNames dict contains: ")
-print(windowNames)
+print("")
 
-# windowName : desktopNum
+print("windowNames: %s" % (windowNames))
+
+print("")
+
+# desktopNum : windowName
 windowLocations = {}
 print("desktops start at 0, you can not create an empty desktop inbetween")
 # desktop apps are being added to
@@ -51,15 +62,23 @@ currentDesktop = 0
 # how many desktops are available
 desktopsAvailable = 0
 # while there are windows that haven't been assigned
-print("add an application to desktop %s OR input NEXT to move to the next desktop" % (currentDesktop))
-while len(windowNames) > 0:
+print("add an application to desktop OR input NEXT to move to the next desktop")
+print("application to choose from:", (windowNames.keys()))
+numberOfApps = len(appList)
+while numberOfApps > 0 :
     appInput = input("desktop %s:   " % (currentDesktop))
     # if the input is a valid windowName
     if appInput in windowNames:
-        # add to windowLocations ~windowName : desktopNum~
-        windowLocations[windowNames[appInput]] = currentDesktop
-        # remove from the possible options to prevent double placing
-        del windowNames[appInput]
+        try:
+            print("Try: windowLocations: ", windowLocations)
+            # add to windowLocations desktopnum : windowName
+            windowLocations[currentDesktop].append( (appInput, windowNames[appInput]) )
+            # remove from the possible options to prevent double placing
+            numberOfApps = numberOfApps - 1
+        except :
+            print(" KeyError windowLocations: ", windowLocations)
+            windowLocations[currentDesktop] = [ ( appInput, windowNames[appInput] ) ]
+            numberOfApps = numberOfApps - 1
         # TODO: CHECK TO SEE IF MULTIPLE OF THE SAME WINDOW WORKS
         # if currentDesktop is the highest available
         if currentDesktop == desktopsAvailable:
@@ -76,5 +95,43 @@ while len(windowNames) > 0:
     # if not valid input
     else:
         print("'%s' is not a valid input" % (appInput))
+    print("")
 
-print(windowLocations)
+print("")
+
+# desktopNum : windowNamesArray
+print("windowLocations: %s" % (windowLocations))
+# dict for windowName : appLaunchcmd
+print("windowNames: %s" % (windowNames))
+# list of launch cmds
+print("appList: %s" % (appList))
+
+
+# windowLocations: {0: [('firefox', 'firefox'), ('atom', 'flatpak run io.atom.Atom')], 1: [('gedit', 'gedit')], 2: [('spotify', 'flatpak run com.spotify.Client')]}
+# windowNames: {'firefox': 'firefox', 'atom': 'flatpak run io.atom.Atom', 'gedit': 'gedit', 'spotify': 'flatpak run com.spotify.Client'}
+# appList: ['firefox', 'flatpak run io.atom.Atom', 'gedit', 'flatpak run com.spotify.Client']
+
+
+# generate code
+
+# get current directory then add the file to generate
+# appGenWindowSetUp.sh
+gen_path = os.path.dirname(os.path.realpath(__file__)) + "/appGenWindowSetUp.sh"
+
+gen_file = open(gen_path, 'w')
+
+gen_file.write("#!/bin/bash")
+gen_file.write("\n \n")
+gen_file.write("# this is a script generated to launch apps in diffrent desktops on start up")
+gen_file.write("\n")
+gen_file.write("\n")
+for desktop in windowLocations:
+    for winTupleToPlace in windowLocations[desktop]:
+        gen_file.write("%s &> /dev/null & sleep %s" % (winTupleToPlace[1], str(waitTime)))
+        gen_file.write("\n")
+        gen_file.write("wmctrl -r %s -t %s" % (winTupleToPlace[0] , desktop))
+        gen_file.write("\n")
+        gen_file.write("\n")
+
+gen_file.write("exit 0")
+gen_file.close()
