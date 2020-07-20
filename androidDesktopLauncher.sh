605950d662241de79522b6a515f61c69c02a9f08
MarvelousMari <mariella@mariellapage.com>
2020-07-20 14:38:34 -0500

    Created and renamed personal desktop launcher scripts

---
+++ androidDesktopLauncher.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# this is a script generated to launch apps in diffrent desktops on start up
+
+firefox &> /dev/null & sleep 2
+wmctrl -r firefox -t 0
+
+flatpak run com.google.AndroidStudio &> /dev/null & sleep 6
+wmctrl -r android -t 1
+
+flatpak run com.spotify.Client &> /dev/null & sleep 2
+wmctrl -r spotify -t 2
+
+exit 0
