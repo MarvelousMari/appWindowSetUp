2e976a0de1b864ccb30bfccb4107365b4fa1bfb2
MarvelousMari <mariella@mariellapage.com>
2020-07-21 14:47:34 -0500

    Customized my personal android desktop launcher

--- androidDesktopLauncher.sh
+++ androidDesktopLauncher.sh
@@ -5,10 +5,13 @@
 firefox udemy.com github.com mariellapage.com/wp-admin &> /dev/null & sleep 2
 wmctrl -r firefox -t 0
 
-flatpak run com.google.AndroidStudio &> /dev/null & sleep 6
+flatpak run com.google.AndroidStudio &> /dev/null & sleep 9
 wmctrl -r android -t 1
 
+github &> /dev/null & sleep 2
+wmctrl -r github -t 2
+
 flatpak run com.spotify.Client &> /dev/null & sleep 2
-wmctrl -r spotify -t 2
+wmctrl -r spotify -t 3
 
 exit 0
