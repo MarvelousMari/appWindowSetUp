4f795d8baad85c5e1c81bee0d8d3be99f2c40373
MarvelousMari <mariella@mariellapage.com>
2020-07-21 11:59:10 -0500

    Updated firefox command to include specific tabs

--- androidDesktopLauncher.sh
+++ androidDesktopLauncher.sh
@@ -2,7 +2,7 @@
 
 # this is a script generated to launch apps in diffrent desktops on start up
 
-firefox &> /dev/null & sleep 2
+firefox udemy.com github.com mariellapage.com/wp-admin &> /dev/null & sleep 2
 wmctrl -r firefox -t 0
 
 flatpak run com.google.AndroidStudio &> /dev/null & sleep 6
