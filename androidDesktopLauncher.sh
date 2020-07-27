368b6dda44b90617edbb530a2d127f1687c16dd5
MarvelousMari <mariella@mariellapage.com>
2020-07-27 11:05:26 -0500

    Updated Android Launch Github URL

--- androidDesktopLauncher.sh
+++ androidDesktopLauncher.sh
@@ -2,13 +2,13 @@
 
 # this is a script generated to launch apps in diffrent desktops on start up
 
-firefox udemy.com github.com mariellapage.com/wp-admin &> /dev/null & sleep 2
+firefox udemy.com github.com/login mariellapage.com/wp-admin &> /dev/null & sleep 2
 wmctrl -r firefox -t 0
 
-flatpak run com.google.AndroidStudio &> /dev/null & sleep 9
+flatpak run com.google.AndroidStudio &> /dev/null & sleep 10
 wmctrl -r android -t 1
 
-github &> /dev/null & sleep 2
+github &> /dev/null & sleep 3
 wmctrl -r github -t 2
 
 flatpak run com.spotify.Client &> /dev/null & sleep 2
